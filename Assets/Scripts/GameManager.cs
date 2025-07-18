8c0ac11798abce81f9f4167c6fd38bf2d891e4ce
Ristwak Pandey <118520927+Ristwak@users.noreply.github.com>
2025-07-18 12:16:31 +0530

    Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject homePanel;
+    public GameObject aboutPanel;
+    public GameObject loadingPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        homePanel.SetActive(true);
+        aboutPanel.SetActive(false);
+        loadingPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartButton()
     {
-        
+        loadingPanel.SetActive(true);
+        SceneManager.LoadScene("GameScene");
+        homePanel.SetActive(false);
+        aboutPanel.SetActive(false);
+        // Load the first scene or start the game logic
+        Debug.Log("Game Started");
+    }
+
+    public void AboutButton()
+    {
+        homePanel.SetActive(false);
+        aboutPanel.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        aboutPanel.SetActive(false);
+        homePanel.SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
     }
 }
